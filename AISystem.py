b6f7433011b6c9af21e2dee7ef9ef4bde98df735
peppe <peppelmn04@gmail.com>
2025-10-28 11:08:10 +0100

    Prova push

--- AISystem.py
+++ AISystem.py
@@ -3,5 +3,5 @@ from DataClassesDefiner import Question
 class AISystem:
     
     def process_question(self, question: Question):
-        # Simula il processamento della domanda da parte del sistema AI
-        return "Simulated answer"
+        # Logica per processare la domanda prova prova proca
+        pass
